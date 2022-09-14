f3279c64c717b0168c1dfca359b9e2f0e4843b0f
Yczar <babalolagbogo@gmail.com>
2022-09-14 21:24:09 +0100

    Where it all begins

---
+++ android/app/src/main/kotlin/com/example/notely/MainActivity.kt
@@ -0,0 +1,6 @@
+package com.example.notely
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity() {
+}
---
+++ lib/main.dart
@@ -0,0 +1,22 @@
+import 'package:flutter/material.dart';
+
+void main() {
+  runApp(const MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  const MyApp({
+    super.key,
+  });
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: 'Notely',
+      theme: ThemeData(
+        primarySwatch: Colors.blue,
+      ),
+      home: const Scaffold(),
+    );
+  }
+}
