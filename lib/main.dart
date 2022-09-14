abe50f040f35e55640955323399b9dc701968ffc
Yczar <babalolagbogo@gmail.com>
2022-09-14 23:07:27 +0100

    updated the main.dart file

--- lib/main.dart
+++ lib/main.dart
@@ -1,22 +1,8 @@
 import 'package:flutter/material.dart';
+import 'package:notely/notely.dart';
 
 void main() {
-  runApp(const MyApp());
-}
-
-class MyApp extends StatelessWidget {
-  const MyApp({
-    super.key,
-  });
-
-  @override
-  Widget build(BuildContext context) {
-    return MaterialApp(
-      title: 'Notely',
-      theme: ThemeData(
-        primarySwatch: Colors.blue,
-      ),
-      home: const Scaffold(),
-    );
-  }
+  runApp(
+    const Notely(),
+  );
 }
